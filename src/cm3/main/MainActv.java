97a4cdc4874e7663bbc47af7e8cce1880c1dc842
Iwabuchi Ken <iwabuchi.k.2010@gmail.com>
2012-10-05 21:16:37 +0900

    B1
    1.1
    
    ---
    1. Up to => Drawable images
    	==> Copied

--- src/cm3/main/MainActv.java
+++ src/cm3/main/MainActv.java
@@ -1,20 +1,77 @@
 package cm3.main;
 
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.app.Activity;
 import android.view.Menu;
 
 public class MainActv extends Activity {
 
+	/*********************************
+	 * Others
+	 *********************************/
+	public static Vibrator vib;
+	
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    	/*********************************
+		 * 1. super
+		 * 2. Set content
+		 * 2-2. Set title
+		 * 3. Initialize => vib
+		 * 
+		 *  4. Set list
+		 *  5. Set listener => Image buttons
+		 *  6. Set path label
+		 *  
+		 *  7. Initialize preferences
+		 *  
+		 *  8. Refresh DB
+
+		 *********************************/
+    	/*********************************
+		 * 1. super
+		 * 2. Set content
+		 *********************************/
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_actv);
-    }
+        
+        /*********************************
+		 * 2-2. Set title
+		 *********************************/
+		this.setTitle(this.getClass().getName());
+        
+        vib = (Vibrator) this.getSystemService(this.VIBRATOR_SERVICE);
+
+//        /*********************************
+//		 * 4. Set list
+//		 *********************************/
+//        set_initial_dir_list();
+//        
+//        /*********************************
+//		 * 5. Set listener => Image buttons
+//		 *********************************/
+//		set_listeners();
+//		
+//		/*********************************
+//		 * 6. Set path label
+//		 *********************************/
+//		Methods.updatePathLabel(this);
+//		
+//		/*********************************
+//		 * 7. Initialize preferences
+//		 *********************************/
+//		init_prefs();
+		
+
+        
+    }//public void onCreate(Bundle savedInstanceState)
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main_actv, menu);
         return true;
     }
-}
+
+}//public class MainActv extends Activity
